dba9aa8b4fb561c1d35b03bd33c7a6583b44b2af
pengxuanyu <pengxuanyus@gmail.com>
2023-02-12 22:41:15 +0800

    策略模式

--- award-service/src/main/java/Test.java
+++ award-service/src/main/java/Test.java
@@ -1,6 +1,10 @@
 import award.strategy.AwardContext;
 import award.strategy.AwardParent;
+import award.strategy.impl.CarAward;
 import award.strategy.impl.PhoneAward;
+import award.vo.AwardInfoVO;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author xuanyu peng
@@ -8,11 +12,17 @@ import award.strategy.impl.PhoneAward;
  * @date 2023/2/12 22:14
  */
 
+@SpringBootApplication
+@EnableDiscoveryClient
 public class Test {
     public static void main(String[] args) {
-        AwardContext context = new AwardContext(new PhoneAward());
-        AwardParent car = context.getAward("car");
-        AwardParent phone = context.getAward("phone");
+        AwardContext phone = new AwardContext(new PhoneAward());
+        AwardContext car = new AwardContext(new CarAward());
+
+        System.out.printf(phone.getAward("phone").doGetAward("phone")+" ");
+        System.out.printf("\n");
+        System.out.printf(car.getAward("car").doGetAward("car")+" ");
+
 
 
     }
