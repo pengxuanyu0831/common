7a1627f72d1b497589a0f4f4f3cd4677a6414e29
pengxuanyu <pengxuanyus@gmail.com>
2023-05-09 23:17:32 +0800

    框架搭建-引入redisson

---
+++ common-config/src/main/java/org/config/redisson/RedissonAutoConfig.java
@@ -0,0 +1,13 @@
+package org.config.redisson;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xuanyu peng
+ * @description:
+ * @date 2023/5/9 23:17
+ */
+@Configuration
+public class RedissonAutoConfig {
+    
+}
