183a1591ad6f0d1663c2088ff6dc48e2088dd96e
pengxuanyu <pengxuanyus@gmail.com>
2023-06-20 23:47:37 +0800

    框架搭建

--- order-service/src/main/java/org/order/OrderSpringApplication.java
+++ order-service/src/main/java/org/order/OrderSpringApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author xuanyu peng
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableTransactionManagement
 //@EnableFeignClients(basePackages = {"org.order.service"})
 public class OrderSpringApplication {
     public static void main(String[] args) {
